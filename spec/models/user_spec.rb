37bcdbad921d99fa3ca0bdced786407763a41d1f
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-12-05 18:22:33 +0200

    Add some tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,8 @@ RSpec.describe User, type: :model do
     it { should have_many(:comments) }
     it { should have_many(:friendships) }
     it { should have_many(:friends).through(:friendships) }
+    it { should have_many(:friend_requests_sent) }
+    it { should have_many(:friend_requests_received) }
+    it { should have_many(:friend_requests).through(:friend_requests_received) }
   end
 end
